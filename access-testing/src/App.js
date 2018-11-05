cb2fa61b9b2116b802736eff09c606991688607e
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-05 18:05:35 -0500

    rm register from app

--- access-testing/src/App.js
+++ access-testing/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import RegisterStudent from './RegisterStudent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RegisterStudent />
       </div>
     );
   }
