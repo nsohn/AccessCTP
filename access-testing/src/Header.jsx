70d4c954505eb76c5a1ab5dbb4b8fab684a23853
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-05 17:45:43 -0500

    header basics

---
+++ access-testing/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+       <div className="header">
+          <h1>Access Your Test</h1>
+       </div>
+     </div>
+     );
+}
+
+export default Header;
