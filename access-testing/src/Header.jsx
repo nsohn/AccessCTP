180ab3ae5d4697df1b71ac37b452feefb798337a
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-12 18:42:34 -0500

    set up the header

--- access-testing/src/Header.jsx
+++ access-testing/src/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import "./Header.css";
+
+/*
+ToDo:
+  * get header content aligned
+  * Add button for "Create Test/Validate Test/Login/Register Depending on the status of the user"
+ */
 
 function Header() {
   return (
-    <div>
-       <div className="header">
-          <h1>Access Your Test</h1>
-       </div>
+     <div className="header">
+        <img src="./hunterLogo.png" alt="Hunter Logo" />
+        <h3 className="header-text">Access Your Test</h3>
+        <button onClick={() => {alert('clicked')}}>Click Here</button>
      </div>
+
      );
 }
 
