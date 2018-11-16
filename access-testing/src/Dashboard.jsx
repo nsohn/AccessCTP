ca74df13bd56763794a43ce8cfa444fa3509143c
Stella Ma <stellama98@gmail.com>
2018-11-15 19:07:47 -0500

    Added changes to Dashboard for student

--- access-testing/src/Dashboard.jsx
+++ access-testing/src/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Jumbotron, Button, Badge} from "react-bootstrap";
 
 class Dashboard extends Component{
   constructor(props){
@@ -10,22 +11,46 @@ class Dashboard extends Component{
     };
   }
 
-  }
+
   render(){
     let typeStudent = this.state.typeStudent
     let typeProfessor = this.state.typeProfessor
     let typeAdmin = this.state.typeAdmin
-    
+
     let userView = <div></div>
     let scheduledTests= <div></div>
 
-    if (typeStudent){
+    if(typeStudent){
       userView =
-      <h1> hello </h1>
+      <div>
+        <h1>Hi Stella!</h1>
+        <p>
+          Badges <Badge>42</Badge>
+        </p>
+      <ListGroup>
+   <ListGroupItem href="#link1">Link 1</ListGroupItem>
+   <ListGroupItem href="#link2">Link 2</ListGroupItem>
+   <ListGroupItem >Trigger an alert</ListGroupItem>
+ </ListGroup>
+ <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button bsStyle="primary">Learn more</Button>
+  </p>
+</Jumbotron>;
+ </div>
+    }
+
     return(
+      <div>
       {userView}
+      </div>
     );
   }
 }
 
-export default Login;
+export default Dashboard;
