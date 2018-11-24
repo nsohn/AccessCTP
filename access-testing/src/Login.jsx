72f40db581c6a12f3ac809e016c04b990feb4cff
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-24 12:09:02 -0500

    added fetch to login

--- access-testing/src/Login.jsx
+++ access-testing/src/Login.jsx
@@ -8,17 +8,34 @@ class Login extends Component {
       password: ''
     }
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
   onChange(e){
     this.setState({
       [e.target.name]: e.target.value
     })
   }
+  onSubmit(){
+    const { userName, password } = this.state;
+    const jsonBody = {
+      user_name: userName,
+      password: password
+    }
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jsonBody)
+    }).then((res) => console.log(res))
+  }
   render(){
     return (
       <div>
-      Email: <input name="userName" onChange={this.onChange} type="email" />
-      Password: <input name="password" onChange={this.onChange} type="password" />
+      {this.state.userName} Email: <input name="userName" onChange={this.onChange} type="email" />
+      {this.state.password}Password: <input name="password" onChange={this.onChange} type="password" />
+      <button onClick={this.onSubmit}>Login</button>
       </div>
     )
   }
