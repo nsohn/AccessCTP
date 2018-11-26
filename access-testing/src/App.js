736db92fbac316d1a0c26999706773843cfb1eed
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-26 17:30:23 -0500

    rm modal from app

--- access-testing/src/App.js
+++ access-testing/src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
-import TestForm from './TestForm';
-import Header from './Header';
 import HomePage from './HomePage';
-
-
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      testModal : false
-    }
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-  closeModal(){
-    this.setState({
-      testModal: false
-    })
-  }
-  openModal(){
-    this.setState({
-      testModal : true
-    })
-  }
   render() {
     return (
       <div className="App">
