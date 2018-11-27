1cdf4dd4179cf63e255e1d70590a4d3a43723976
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2018-11-26 19:04:23 -0500

    set up router

--- access-testing/src/App.js
+++ access-testing/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import Header from './Header';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <HomePage />
+      <Header />
+      <Router>
+        <Route exact path='/' render={ props => 
+          <HomePage />
+        }/>
+      </Router>
       </div>
     );
   }
